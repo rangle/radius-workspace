37cab0615d6b142a1e59b1b394d6d0d9c7df3690
DogaIster <doga.ister@rangle.io>
2022-03-07 16:03:44 -0500

    Refer to comment and changed let to const

--- packages/cli/src/commands/init/utils/inquirer.spec.ts
+++ packages/cli/src/commands/init/utils/inquirer.spec.ts
@@ -3,23 +3,24 @@ import {filter, isReact, validateDesignSystemNameAnswer} from './inquirer';
 describe('Inquirer', () => {
 
     it('should accept a design system name', () => {
-        let designSystemName = 'This is an awesome test';
+        const designSystemName = 'This is an awesome test';
         expect(validateDesignSystemNameAnswer(designSystemName)).toBe(true);
     });
 
     it('should prompt the question again if design system name is empty or only white space', () => {
-        let designSystemNameEmpty = '';
-        let designSystemNameEmptySpace = ' '
+        const designSystemNameEmpty = '';
+        const designSystemNameEmptySpace = ' '
         expect(validateDesignSystemNameAnswer(designSystemNameEmpty)).toBe('Please enter the name of the design system');
         expect(validateDesignSystemNameAnswer(designSystemNameEmptySpace)).toBe('Please enter the name of the design system');
     });
 
     it('should make everything lower case', () => {
-        let val = "I AM ALL CAPS, MAKE ME LOWER CASE";
+        const val = "I AM ALL CAPS, MAKE ME LOWER CASE";
         expect(filter(val)).toBe("i am all caps, make me lower case");
     });
 
     it('should check if the answer is react', () => {
-        expect(false).toBe(isReact('react'));
+        expect(isReact('react')).toBeTruthy();
     });
+
 });
