bca5f7e11674e38b75c47154bbc1436e17a51789
DogaIster <doga.ister@rangle.io>
2022-03-09 13:56:34 -0500

    added more tests and made the repo code more readable

--- packages/cli/src/commands/init/utils/repo.spec.ts
+++ packages/cli/src/commands/init/utils/repo.spec.ts
@@ -1,4 +1,4 @@
-import {configureGitSetup} from "./repo";
+import {configureGitSetup, logFailure, logSuccess, selectRepo} from "./repo";
 
 describe('Repo', () => {
 
@@ -35,4 +35,39 @@ describe('Repo', () => {
             expect(removeDir.mock.calls.length).toBe(1);
         }
     });
+
+    it('should return correct repo for Angular', () => {
+       const angularRepo = selectRepo({'ds-framework': 'angular'});
+        expect(angularRepo).toStrictEqual({"branch": "main", "repo": "rangle/radius-angular"});
+    });
+
+    it('should return correct repo for React', () => {
+        const reactRepo = selectRepo({'ds-framework': 'react["css"]'});
+        expect(reactRepo).toBeDefined();
+    });
+
+    it('should return false if selection is empty', () => {
+        const repo = selectRepo({});
+        const logSpy = jest.spyOn(console, 'log');
+        console.log('coming soon... 😉');
+        expect(repo).toBeFalsy()
+        expect(logSpy).toHaveBeenCalledWith('coming soon... 😉');
+    });
+
+    it('should log when creation is successful', () => {
+        const logSpy = jest.spyOn(console, 'log');
+        logSuccess({'ds-framework': 'angular'});
+        expect(logSpy).toBeDefined();
+    });
+
+    it('should log when creation is NOT successful', () => {
+        const logSpy = jest.spyOn(console, 'log');
+        let error: any = '';
+        console.log('Couldn\'t clone the repo.');
+        logFailure(error);
+        expect(logSpy).toBeDefined();
+        expect(logSpy).toHaveBeenCalledWith('Couldn\'t clone the repo.');
+    });
+
+
 });
