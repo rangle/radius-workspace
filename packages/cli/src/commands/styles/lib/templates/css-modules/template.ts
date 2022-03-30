3febec95138193a26927577da16ccebf4beee21e
rafRangler <97181940+rafRangler@users.noreply.github.com>
2022-03-30 15:13:51 -0400

    Feature/r20 69 (#19)
    * added method generateNodes() for creating tokens and  processSpacingNode() that takes a NodeDocument object
    
    * removed extra type Anystyle from figma.utils.ts
    
    * removed package.lock.json
    
    * added method processElevationToken to generate new token set
    
    * removed map for gridBreakpointTokens within generateTokensV2
    
    * removed return statement, to prevent V2 tokens from being generated
    
    * turned eslint rule off for duplicated object key  name within function block
    
    * fix(figma.utils.ts): the elevation tokens are updated to the correct format
    
    * style(figma.utils.ts): formatting
    
    * refactor: removed cloned repo directory
    
    * refactor(root.ts): resolving merge conflicts with elevation method call R20-85

--- packages/cli/src/commands/styles/lib/templates/css-modules/template.ts
+++ packages/cli/src/commands/styles/lib/templates/css-modules/template.ts
@@ -1,26 +1,26 @@
 import { RenderTokenFile, TOKEN_FILE_COMMENTS } from '../types';
 
 export const template: RenderTokenFile = (tokens, type) =>
-	[
+  [
 	    `./styles/_${ type }.css`,
 	    `${ TOKEN_FILE_COMMENTS[type] }
     ${ tokens.map(({ token, value }) => `  ${ token }: ${ value };`).join('\n') }`
-	] as const;
+  ] as const;
 
 export const color: RenderTokenFile = (tokens, type) =>
-	[
+  [
 	    `./styles/_${ type }.css`,
 	    `:root {
 ${ TOKEN_FILE_COMMENTS[type] }
 ${ tokens.map(({ token, value }) => `  ${ token }: ${ value };`).join('\n') }
 }`
-	] as const;
+  ] as const;
 
 export const spacing: RenderTokenFile = (tokens, type) =>
-	[
+  [
 	    `./styles/_${ type }.css`,
 	    `:root {
 ${ TOKEN_FILE_COMMENTS[type] }
 ${ tokens.map(({ token, value }) => `  ${ token }: '${ value }px';`).join('\n') }
 }`
-	] as const;
+  ] as const;
