5c76319ae91611f4089bc0ec38f8c1392cb94eae
DogaIster <doga.ister@rangle.io>
2022-04-13 15:15:31 -0400

    chore: wrapped in if clause
    wrapped in if clause

--- packages/cli/src/commands/styles/utils/figma.utils.ts
+++ packages/cli/src/commands/styles/utils/figma.utils.ts
@@ -38,9 +38,11 @@ export type FigmaData = {
 };
 
 export const assert = <T>(x: T, msg?: string) => {
-  if (!x) throw new Error(msg || 'Assertion failed');
-  console.log('Please make sure you entered a valid URL and token and try again');
-  process.exit(1);
+  if (!x) {
+    throw new Error(msg || 'Assertion failed');
+    console.log('Please make sure you entered a valid URL and token and try again');
+    process.exit(1);
+  }
 };
 
 export function assertFigmaData(d: unknown): asserts d is FigmaData {
